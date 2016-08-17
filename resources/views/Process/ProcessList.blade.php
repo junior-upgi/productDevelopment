85dd3975631d1cfd8fb9d0cfcab8e452f79e4410
林于斌 <papago75@gmail.com.tw>
2016-08-17 17:00:14 +0800

    1、調整工期與完成時間之時間顯示方式 2、判斷工期與完成時間點，依情況顯示註記

--- resources/views/Process/ProcessList.blade.php
+++ resources/views/Process/ProcessList.blade.php
@@ -10,11 +10,12 @@
         <li class="active">產品開發流程</li>
     </ol>
     <!--product info panel-->
+    {{--*/ $Deadline = strtotime($ProductData->deadline) /*--}}
     <p class="bg-info">
         <span style="margin-right:20px;">產品代碼：{{$ProductData->referenceNumber}}</span>
         <span style="margin-right:20px;">產品名稱：{{$ProductData->referenceName}}</span>
         <span style="margin-right:20px;">需求數量：{{$ProductData->requiredQuantity}}</span>
-        <span style="margin-right:20px;">交貨期限：{{date('Y-m-d', strtotime($ProductData->deadline))}}</span>   
+        <span style="margin-right:20px;">交貨期限：{{date('Y-m-d', $Deadline)}}</span>   
     </p>
     <!--tool bar-->
     <nav class="navbar navbar-default" role="navigation">
@@ -36,7 +37,7 @@
                 <td>名稱</td>
                 <td>負責人</td>
                 <td class="col-md-1">工時</td>
-                <td class="col-md-3">工期</td>
+                <td width="200">工期</td>
                 <td width="100" class="text-center">完成時間</td>
                 <td width="60"></td>
             </tr>
@@ -44,9 +45,13 @@
         <tbody id="tableSort">
             {{--*/ $CostCount = 0 /*--}}
             {{--*/ $i = 1 /*--}}
+            {{--*/ use Carbon\Carbon /*--}}
+            {{--*/ $Now = strtotime(Carbon::now() . '-1 day') /*--}}
             @foreach($ProcessList as $list)
-                {{--*/ $StartDate = $CostCount /*--}}
-                {{--*/ $EndDate = ($CostCount += $list->timeCost) - 1 /*--}}
+                {{--*/ $StartDays = $CostCount /*--}}
+                {{--*/ $EndDays = ($CostCount += $list->timeCost) - 1 /*--}}
+                {{--*/ $StartDate = strtotime($ProductData->startDate . '+' . $StartDays . ' day') /*--}}
+                {{--*/ $EndDate = strtotime($ProductData->startDate . '+' . $EndDays . ' day') /*--}}
                 @if($list->complete == "0")
                     <tr id="{{$list->ID}}">
                 @else
@@ -73,16 +78,49 @@
                         <span>{{$list->timeCost}}</span>
                     </td>
                     <td>
-                        <span>{{date('Y-m-d', strtotime($ProductData->startDate . '+' . $StartDate . ' day'))}} 
-                            ~ {{date('Y-m-d', strtotime($ProductData->startDate . '+' . $EndDate . ' day'))}}</span>
+                        @if($StartDate > $Deadline)
+                            <span class="label label-danger">
+                                {{date('Y-m-d', $StartDate)}}
+                            </span>
+                        @else
+                            <span>
+                                {{date('Y-m-d', $StartDate)}}
+                            </span>
+                        @endif
+                        <span>~</span>
+                        @if($EndDate > $Deadline)
+                            <span class="label label-danger">
+                                {{date('Y-m-d', $EndDate)}}
+                            <span>
+                        @else
+                            <span>
+                                {{date('Y-m-d', $EndDate)}}
+                            <span>
+                        @endif 
                     </td>
                     <td class="text-center">
                         @if($list->complete == "0")
-                            <button type="button" class="btn btn-sm btn-default" onclick="Complete('{{$list->ID}}')">完成</button>
+                            @if($Now > $Deadline)
+                                <button type="button" class="btn btn-sm btn-danger" onclick="Complete('{{$list->ID}}')">完成</button>
+                            @elseif($Now > $EndDate)
+                                <button type="button" class="btn btn-sm btn-warning" onclick="Complete('{{$list->ID}}')">完成</button>
+                            @else
+                                <button type="button" class="btn btn-sm btn-default" onclick="Complete('{{$list->ID}}')">完成</button>
+                            @endif
                         @else
-                            <span class="label label-danger">
-                                {{date('Y-m-d', strtotime($list->completeTime))}}
-                            </span>
+                            @if($Now > $Deadline)
+                                <span class="label label-danger">
+                                    {{date('Y-m-d', strtotime($list->completeTime))}}
+                                </span>
+                            @elseif($Now > $EndDate)
+                                <span class="label label-warning">
+                                    {{date('Y-m-d', strtotime($list->completeTime))}}
+                                </span>
+                            @else
+                                <span class="label label-success">
+                                    {{date('Y-m-d', strtotime($list->completeTime))}}
+                                </span>
+                            @endif
                         @endif
                     </td>
                     <td>
