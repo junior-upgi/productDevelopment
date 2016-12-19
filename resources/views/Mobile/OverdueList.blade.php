984bacd88af28c102b8ca82008bb8911f6ec7347
林于斌 <papago75@gmail.com.tw>
2016-12-19 08:25:22 +0800

    修正沒有逾期開發案時逾期清單顯示提示

--- resources/views/Mobile/OverdueList.blade.php
+++ resources/views/Mobile/OverdueList.blade.php
@@ -3,24 +3,30 @@
     <div class="text-center">
         <h3>開發案逾期清單</h3>
     </div>
-    <div class="list-group">
-        @foreach($overdueList as $list)
-            @php
-                if (strtotime($list->nowEndDate > $list->endDate)) {
-                    $class = 'list-group-item list-group-item-danger';
-                } else {
-                    $class = 'list-group-item list-group-item-warning';
-                }
-            @endphp
-            <a class="{{ $class }}">
-                <h4>{{ "[$list->projectNumber]$list->projectName" }}</h4>
-                <p>
-                    {{ "產品: [$list->referenceNumber]$list->referenceName" }}<br/>
-                    {{ "顧客: $list->clientName" }}<br/>
-                    {{ "業務: $list->salesName" }}<br/>
-                    {{ "完工期限: " . date('Y-m-d', strtotime($list->deadline)) }}
-                </p>
-            </a>
-        @endforeach
-    </div>
+    @if (count($overdueList) > 0)
+        <div class="list-group">
+            @foreach ($overdueList as $list)
+                @php
+                    if (strtotime($list->nowEndDate > $list->endDate)) {
+                        $class = 'list-group-item list-group-item-danger';
+                    } else {
+                        $class = 'list-group-item list-group-item-warning';
+                    }
+                @endphp
+                <a class="{{ $class }}">
+                    <h4>{{ "[$list->projectNumber]$list->projectName" }}</h4>
+                    <p>
+                        {{ "產品: [$list->referenceNumber]$list->referenceName" }}<br/>
+                        {{ "顧客: $list->clientName" }}<br/>
+                        {{ "業務: $list->salesName" }}<br/>
+                        {{ "完工期限: " . date('Y-m-d', strtotime($list->deadline)) }}
+                    </p>
+                </a>
+            @endforeach
+        </div>
+    @else
+        <div class="alert alert-info" role="alert">
+            目前沒有任何逾期的開發案!!
+        </div>
+    @endif
 @endsection
